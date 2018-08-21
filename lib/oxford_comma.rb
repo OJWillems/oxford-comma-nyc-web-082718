a7174da284564ec79553864645c0247d60f02da9
Olivier Willems <ojwillems@gmail.com>
2018-08-21 23:38:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,33 +6,20 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   else
-    
-    
-#     x = 0
-#     while x < array.size
-#       if x < array.size - 2
-#         array.insert(x + 1, ", ")
-#       else
-#         array.insert (x + 1, ", and ")
-#       end
-#     end
-#     array.join
-#   end
-# end
-    
-    # first_array = []
-    # second_array = []
-    # x = 0
-    # while x < array.size
-    #   if x < array.size - 2
-    #     first_array.push(array[x])
-    #     x += 1
-    #   else
-    #     second_array.push(array[x])
-    #     x += 1
-    #   end
-    # end
-    # first_array.join(", ")
-    # second_array.join(", and ")
-    # first_array + second_array
-
+    first_array = []
+    second_array = []
+    x = 0
+    while x < array.size
+      if x < array.size - 2
+        first_array.push(array[x])
+        x += 1
+      else
+        second_array.push(array[x])
+        x += 1
+      end
+    end
+    first_array.join(", ")
+    second_array.join(", and ")
+    first_array + second_array
+  end
+end
