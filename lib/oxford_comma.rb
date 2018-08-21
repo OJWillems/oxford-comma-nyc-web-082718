ca7848099a6e74a78e2dfe810ae54f40fdca859e
Olivier Willems <ojwillems@gmail.com>
2018-08-21 22:49:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  if array.size == 2
+  if array.size < 2
     array.join
+  elsif array.size == 2
+    array.join(" and ")
   else
     array.join
-  array.join
-  array.join(" and ")
-  array.join()
+  end
 end
