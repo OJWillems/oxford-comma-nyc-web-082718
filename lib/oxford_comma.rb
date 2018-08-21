771f2231e11d91cec41da4cb0e000f7391a78c1a
Olivier Willems <ojwillems@gmail.com>
2018-08-21 23:38:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -23,3 +23,15 @@ def oxford_comma(array)
     first_array + second_array
   end
 end
+
+x = 0
+    while x < array.size
+      if x < array.size - 2
+        array.insert(x + 1, ", ")
+      else
+        array.insert (x + 1, ", and ")
+      end
+    end
+    array.join
+  end
+end
