f64f9f46776c721f997c881be16856c7a7ab8fa3
Olivier Willems <ojwillems@gmail.com>
2018-08-21 23:23:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,17 +7,17 @@ def oxford_comma(array)
     array.join(" and ")
   else
     first_array = []
-    x = 0
-    while x < array.size - 2
-      first_array.push(array[x])
-      x += 1
-    end
-    first_array.join(", ")
     second_array = []
-    while x >= array.size - 2
-      second_array.push(array[x])
-      x += 1
-    end
+    x = 0
+    while x < array.size
+      if x < array.size - 2
+        first_array.push(array[x])
+        x += 1
+      else
+        second_array.push(array[x])
+        x += 1
+      end
+    first_array.join(", ")
     second_array.join(", and ")
     return first_array + second_array
   end
