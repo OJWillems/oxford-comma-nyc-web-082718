0358e1eec287b07995f9c4bc34873e7f85aaefc0
Olivier Willems <ojwillems@gmail.com>
2018-08-21 22:52:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,4 @@
 def oxford_comma(array)
-  if array.size < 2
-    array.join
-  elsif array.size == 2
-    array.join(" and ")
-  else
-    array.join
-  end
-end
-
-
-def oxford_comma(array)
   array.join
-  array.join(" and ")
+  # array.join(" and ")
 end
